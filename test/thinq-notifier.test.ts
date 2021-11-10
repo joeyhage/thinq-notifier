79f97430938eb6d9fd763484741f70adac181c81
Joey Hage <joeyhage@outlook.com>
2021-11-10 13:44:05 -0600

    Initial commit

---
+++ test/thinq-notifier.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as ThinqNotifier from '../lib/thinq-notifier-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/thinq-notifier-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ThinqNotifier.ThinqNotifierStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
