40c9abf0853bb6701dde51bbec57d4ad968bc127
Joey Hage <joeyhage@outlook.com>
2021-11-12 08:58:38 -0600

    fix datetime issue causing extra notifications (#5)

---
+++ lambda/datetime-util.ts
@@ -0,0 +1,24 @@
+
+export function formatDate(date: Date, timezoneCode?: string): string {
+  console.log(date, timezoneCode);
+  return new Intl.DateTimeFormat("default", {
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+    timeZone: timezoneCode,
+    timeZoneName: "short",
+  }).format(date);
+}
+
+export function determineThresholdDatetime(eventDate: Date): Date {
+  const thresholdHours = Number(process.env.NOTIFICATION_THRESHOLD_HRS);
+  const thresholdDatetime = new Date(eventDate.getTime());
+  thresholdDatetime.setHours(eventDate.getHours() + thresholdHours);
+  return thresholdDatetime;
+}
+
+export function hasThresholdTimePassed(thresholdDatetime: Date): boolean {
+  return Date.now() > thresholdDatetime.getTime();
+}
---
+++ test/index.test.ts
@@ -0,0 +1,87 @@
+import { shouldSendRepeatNotification } from "../lambda/index";
+
+describe("test shouldSendRepeatNotification given `NOTIFICATION_FREQ_HRS`=3 and `MAX_NOTIFICATIONS`=2", () => {
+
+  beforeEach(() => {
+    process.env.NOTIFICATION_FREQ_HRS = "3";
+    process.env.MAX_NOTIFICATIONS = "2";
+  });
+
+  test("should send repeat notification given washer finished 3 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 3);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(true);
+  });
+
+  test("should send repeat notification given washer finished 6 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 6);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(true);
+  });
+
+  test("should not send repeat notification given washer finished 9 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 9);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(false);
+  });
+
+  test("should not send repeat notification given washer finished 9 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 9);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(false);
+  });
+
+  test("should not send repeat notification given washer finished 4 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 4);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(false);
+  });
+
+  test("should not send repeat notification given washer finished 7 hours ago", async () => {
+    // given
+    const thresholdTime = new Date();
+    thresholdTime.setHours(thresholdTime.getHours() - 7);
+    thresholdTime.setMinutes(thresholdTime.getMinutes() - 20);
+
+    // when
+    const result = shouldSendRepeatNotification(thresholdTime);
+
+    // then
+    expect(result).toBe(false);
+  });
+});
